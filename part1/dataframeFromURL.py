4750294e27850beb3605b3ee8fae00284eb95f91
David Clancy <djclancy@uw.edu>
2019-10-23 16:04:27 -0700

    commented code

--- part1/dataframeFromURL.py
+++ part1/dataframeFromURL.py
@@ -20,15 +20,18 @@ def randString_withCSV(length=10):
 	st = ''.join(random.choice(letts) for j in range(0,length))
 	return st+".csv"
 
-
+# does part 2
 def test_create_dataframe(df, cols):
 	c = df.columns
 	c.sort()
 	cols.sort()
+# see if the sorted columns are the same
 	if c != cols:
 		return False
+# find out if there are fewer than 10 indices
 	elif len(df.index) < 10:
 		return False
+# find out if the columns are each of the same type
 	else:
 		sameColTypes = [str(df[y].dtype) != 'object' for y in c]
 		return (sum(sameColTypes) == len(sameColTypes))
